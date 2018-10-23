d1f5e00b0009a502b0c4806219a5377e29a709aa
Nanang Jaya <njaya531@gmail.com>
2018-10-23 08:49:17 +0700

    Kalkulator Quiz

---
+++ kalkulator.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Untitled Form</title>
+<link rel="stylesheet" type="text/css" href="view.css" media="all">
+<script type="text/javascript" src="view.js"></script>
+
+</head>
+<body id="#main_body" >
+	
+	<img id="top" src="top.png" alt="">
+	<div id="form_container">
+	
+		<h1><a>Untitled Form</a></h1>
+		<form id="form_32623" class="appnitro"  method="post" action="">
+					<div class="form_description">
+			
+			
+		 <div id="login">
+  <form  name="kalkulator" action="bmi.php" method="POST">
+<font size="3">    <center><h2><font size="7"><b><font color="#C0FF37"><font size="3"><font size="5"><font color="#FF4530">KALKULATOR BMI</font></font></font></font></b></font></h2></center><hr></font>
+    <div class="field_container">
+      <input type="text" placeholder="Tinggi" name="tinggi"height="120px" >
+    </div>
+<br>
+    <div class="field_container">
+      <input type="text" placeholder="Berat" name="berat"">
+      
+    </div>
+     <div class="field_container">
+      <center>
+      <button id="sign_in_button" name="hitung_bmi"  type="submit">
+        <span class="button_text">Hitung BMI</span>
+      </button>
+      </center>
+    </div>
+
+</form>
+
+ 
+</div>
+<script type="text/javascript" src="../function.js"></script>
+
+	</body>
+</html>
